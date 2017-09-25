a39bf2bc1514f4dbe5ba62c0ae97c5400a0761d7
mhalev <31754531+mhalev@users.noreply.github.com>
2017-09-25 23:00:55 +0700

    Delete listofstring.py

--- listofstring.py
+++
@@ -1,9 +0,0 @@
-from random import randrange
-
-def list_int_to_str(list_of_int):
-
-	return list(map(str,list_of_int))
-
-list_of_int = [randrange(1,10) for _ in range(randrange(5,10))]
-
-print('Список чисел: ',list_of_int,'\n'+'\r','Список строк: ', list_int_to_str(list_of_int))
